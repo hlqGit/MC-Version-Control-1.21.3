c49d6deabf8cc416cc3d5009ec55dee75f21eb99
hlq <justinjoslin@proton.me>
2024-11-09 15:22:03 -0600

    Initial commit

---
+++ src/main/java/dev/hlq/versioncontrol/MCVersionControl.java
@@ -0,0 +1,15 @@
+package dev.hlq.versioncontrol;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCVersionControl implements ModInitializer {
+	public static final String MOD_ID = "mc-version-control";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/dev/hlq/versioncontrol/MCVersionControlClient.java
@@ -0,0 +1,10 @@
+package dev.hlq.versioncontrol;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCVersionControlClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
